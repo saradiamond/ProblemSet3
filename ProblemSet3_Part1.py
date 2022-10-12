34c8203240d60e984bfc6f24a51aa276c6963a9e
saradiamond <sara.diamond@duke.edu>
2022-10-12 13:51:57 -0400

    Initial commit

---
+++ ProblemSet3_Part1.py
@@ -0,0 +1,12 @@
+#/*-PS3: Code Block 1--*/
+#%% Task 1
+
+mountain = "Denali"
+nickname = 'Mt. McKinley'
+elevation = '20322'
+
+print (mountain + ', formerly \nknown as"' + nickname + '"')
+print ("is " + elevation + "'above sea level.")
+#%% Task 2
+
+
